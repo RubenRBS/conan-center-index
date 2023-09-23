11fe8138cb98a3b03ac0c5d714f27777d3bf3dd3
Rubén Rincón <rubenrb@jfrog.com>
2023-09-23 15:13:06 +0200

    Bump scip's zlib requirement to [>=1.2.11 <2] range

--- recipes/scip/all/conanfile.py
+++ recipes/scip/all/conanfile.py
@@ -80,7 +80,7 @@ class SCIPConan(ConanFile):
         if self.options.with_sym == "bliss":
             self.requires("bliss/0.77")
         self.requires("soplex/6.0.3")
-        self.requires("zlib/1.2.13")
+        self.requires("zlib/[>=1.2.11 <2]")
 
     def configure(self):
         self.options["soplex"].with_gmp = self.options.with_gmp
